54b044e498eaa24494b31ae32b334686a08d0f2d
FUJI Goro (gfx) <gfuji@cpan.org>
2019-04-28 15:23:57 +0900

    clean all

--- test/1byte.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fileName = "1byte.bin";
-
-header('Content-Type: application/octet-stream');
-//header('Content-Disposition: attachment; filename=dl.zip');
-header('Content-Length: ' . filesize($fileName));
-readfile($fileName);
-?>
--- test/1mb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fileName = "1mb.bin";
-header('Content-Type: application/octet-stream');
-//header('Content-Disposition: attachment; filename=dl.zip');
-header('Content-Length: ' . filesize($fileName));
-readfile($fileName);
-?>
