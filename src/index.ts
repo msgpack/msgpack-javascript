39535bd8c9b191cca8de281647fdf0c161b683cd
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-01 22:21:11 +0900

    cleanup public APIs exported in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { BufferType } from "./BufferType";
 export { encode } from "./encode";
 export { decode } from "./decode";
+export { ExtensionCodec, ExtensionCodecType } from "./ExtensionCodec";
