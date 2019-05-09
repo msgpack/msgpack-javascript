b558546ed54f207eccd3d65019728e0c29663fb3
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-09 09:58:58 +0900

    add tests

--- test/codec-int.test.ts
+++ test/codec-int.test.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { setInt64, getInt64 } from "../src/utils/int";
+import { setInt64, getInt64, getUint64, setUint64 } from "../src/utils/int";
 
 const INT64SPECS = {
   ZERO: 0,
@@ -13,17 +13,33 @@ const INT64SPECS = {
   MIN_SAFE_INTEGER: Number.MIN_SAFE_INTEGER,
 } as Record<string, number>;
 
-describe("codec: encode and decode int 32/64", () => {
+describe("codec: int64 / uint64", () => {
   context("int 64", () => {
     for (const name of Object.keys(INT64SPECS)) {
       const value = INT64SPECS[name];
 
-      it(`${value} (${value < 0 ? "-" : ""}0x${Math.abs(value).toString(16)})`, () => {
+      it(`sets and gets ${value} (${value < 0 ? "-" : ""}0x${Math.abs(value).toString(16)})`, () => {
         const b = new Uint8Array(8);
         const view = new DataView(b.buffer);
-        setInt64(new DataView(b.buffer), 0, value);
+        setInt64(view, 0, value);
         assert.deepStrictEqual(getInt64(view, 0), value);
       });
     }
+  });
+
+  context("uint 64", () => {
+    it(`sets and gets 0`, () => {
+      const b = new Uint8Array(8);
+      const view = new DataView(b.buffer);
+      setUint64(view, 0, 0);
+      assert.deepStrictEqual(getUint64(view, 0), 0);
+    });
+
+    it(`sets and gets MAX_SAFE_INTEGER`, () => {
+      const b = new Uint8Array(8);
+      const view = new DataView(b.buffer);
+      setUint64(view, 0, Number.MAX_SAFE_INTEGER);
+      assert.deepStrictEqual(getUint64(view, 0), Number.MAX_SAFE_INTEGER);
+    });
   });
 });
