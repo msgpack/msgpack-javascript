834107a0dd8ee902803f3836837519c85f26babc
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-11 19:31:37 +0900

    comments

--- sauceLabs.ts
+++ sauceLabs.ts
@@ -15,6 +15,9 @@ export const sauceLabs = {
   recordVideo: true,
   recordScreenshots: true,
   maxDuration: 120,
+
+  // Only master branch are logged to the SauceLabs builds, which updates the browser-matrix badge.
+  // build: process.env.TRAVIS_BRANCH === "master" ? process.env.TRAVIS_BUILD_NUMBER : undefined,
   build: process.env.TRAVIS_BUILD_NUMBER,
 };
 
