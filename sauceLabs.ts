4338f9517a75af1ff60eeca1f49cd01d93f91b75
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-11 22:47:31 +0900

    remove useless settings

--- sauceLabs.ts
+++ sauceLabs.ts
@@ -15,7 +15,6 @@ export const sauceLabs = {
   tags: ["msgpack-javascript"],
   recordVideo: IS_LOCAL,
   recordScreenshots: IS_LOCAL,
-  maxDuration: 120,
 
   // Only master branch are logged to the SauceLabs builds, which updates the browser-matrix badge.
   build: process.env.TRAVIS_BRANCH === "master" ? process.env.TRAVIS_BUILD_NUMBER : undefined,
