256db9e8f67e8ceeb77ac569db9be1c306ee5214
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-18 23:01:32 +0900

    implement max${Type}Length to safely decode untrusted bytes

--- src/decodeAsync.ts
+++ src/decodeAsync.ts
@@ -1,16 +1,53 @@
 import { ExtensionCodecType } from "./ExtensionCodec";
 import { Decoder } from "./Decoder";
+import { defaultDecodeOptions } from "./decode";
 
-type DecodeAsyncOptions = Partial<
+// the same structure as DecodeOptions but has different doc.
+export type DecodeAsyncOptions = Partial<
   Readonly<{
     extensionCodec: ExtensionCodecType;
+
+    /**
+     * Maximum string length.
+     * Default to 4_294_967_295 (UINT32_MAX).
+     */
+    maxStrLength: number;
+    /**
+     * Maximum binary length.
+     * Default to 4_294_967_295 (UINT32_MAX).
+     */
+    maxBinLength: number;
+    /**
+     * Maximum array length.
+     * Default to 4_294_967_295 (UINT32_MAX).
+     */
+    maxArrayLength: number;
+    /**
+     * Maximum map length.
+     * Default to 4_294_967_295 (UINT32_MAX).
+     */
+    maxMapLength: number;
+    /**
+     * Maximum extension length.
+     * Default to 4_294_967_295 (UINT32_MAX).
+     */
+    maxExtLength: number;
   }>
 >;
 
+export const defaultDecodeAsyncOptions = defaultDecodeOptions;
+
 export async function decodeAsync(
   stream: AsyncIterable<Uint8Array | ArrayLike<number>>,
-  options?: DecodeAsyncOptions,
+  options: DecodeAsyncOptions = defaultDecodeOptions,
 ): Promise<unknown> {
-  const decoder = new Decoder(options && options.extensionCodec);
+  const decoder = new Decoder(
+    options.extensionCodec,
+    options.maxStrLength,
+    options.maxBinLength,
+    options.maxArrayLength,
+    options.maxMapLength,
+    options.maxExtLength,
+  );
   return decoder.decodeOneAsync(stream);
 }
---
+++ test/decode-max-length.test.ts
@@ -0,0 +1,77 @@
+import assert from "assert";
+import { encode, decode, decodeAsync } from "../src";
+import { DecodeOptions } from "../src/decode";
+
+describe("decode with max${Type}Length specified", () => {
+  async function* createStream<T>(input: T) {
+    yield input;
+  }
+
+  context("maxStrLength", () => {
+    const input = encode("foo");
+    const options: DecodeOptions = { maxStrLength: 1 };
+
+    it("throws errors (synchronous)", () => {
+      assert.throws(() => {
+        decode(input, options);
+      }, /max length exceeded/i);
+    });
+
+    it("throws errors (asynchronous)", async () => {
+      await assert.rejects(async () => {
+        await decodeAsync(createStream(input), options);
+      }, /max length exceeded/i);
+    });
+  });
+
+  context("maxBinLength", () => {
+    const input = encode(Uint8Array.from([1, 2, 3]));
+    const options: DecodeOptions = { maxBinLength: 1 };
+
+    it("throws errors (synchronous)", () => {
+      assert.throws(() => {
+        decode(input, options);
+      }, /max length exceeded/i);
+    });
+
+    it("throws errors (asynchronous)", async () => {
+      await assert.rejects(async () => {
+        await decodeAsync(createStream(input), options);
+      }, /max length exceeded/i);
+    });
+  });
+
+  context("maxArrayLength", () => {
+    const input = encode([1, 2, 3]);
+    const options: DecodeOptions = { maxArrayLength: 1 };
+
+    it("throws errors (synchronous)", () => {
+      assert.throws(() => {
+        decode(input, options);
+      }, /max length exceeded/i);
+    });
+
+    it("throws errors (asynchronous)", async () => {
+      await assert.rejects(async () => {
+        await decodeAsync(createStream(input), options);
+      }, /max length exceeded/i);
+    });
+  });
+
+  context("maxMapLength", () => {
+    const input = encode({ foo: 1, bar: 1, baz: 3 });
+    const options: DecodeOptions = { maxMapLength: 1 };
+
+    it("throws errors (synchronous)", () => {
+      assert.throws(() => {
+        decode(input, options);
+      }, /max length exceeded/i);
+    });
+
+    it("throws errors (asynchronous)", async () => {
+      await assert.rejects(async () => {
+        await decodeAsync(createStream(input), options);
+      }, /max length exceeded/i);
+    });
+  });
+});
