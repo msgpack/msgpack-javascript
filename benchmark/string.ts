0d6b2f4a676df6df9a0b08deaf3bf16f4941bfe5
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-23 11:41:10 +0900

    benchmark tweaks

--- benchmark/string.ts
+++ benchmark/string.ts
@@ -1,22 +1,50 @@
 /* eslint-disable no-console */
 import { encode, decode } from "../src";
 
-const data = "Hello, 🌏\n".repeat(10000);
+const ascii = "A".repeat(40000);
+const emoji = "🌏".repeat(20000);
 
-// warm up
-const encoded = encode(data);
-decode(encoded);
-
-// run
-
-console.time("encode");
-for (let i = 0; i < 1000; i++) {
-  encode(data);
-}
-console.timeEnd("encode");
-
-console.time("decode");
-for (let i = 0; i < 1000; i++) {
+{
+  // warm up ascii
+  const data = ascii;
+  const encoded = encode(data);
   decode(encoded);
+  console.log(`encode / decode ascii data.length=${data.length} encoded.byteLength=${encoded.byteLength}`);
+
+  // run
+
+  console.time("encode ascii");
+  for (let i = 0; i < 1000; i++) {
+    encode(data);
+  }
+  console.timeEnd("encode ascii");
+
+  console.time("decode ascii");
+  for (let i = 0; i < 1000; i++) {
+    decode(encoded);
+  }
+  console.timeEnd("decode ascii");
 }
-console.timeEnd("decode");
+
+{
+  // warm up emoji
+  const data = emoji;
+  const encoded = encode(data);
+  decode(encoded);
+
+  console.log(`encode / decode emoji data.length=${data.length} encoded.byteLength=${encoded.byteLength}`);
+
+  // run
+
+  console.time("encode emoji");
+  for (let i = 0; i < 1000; i++) {
+    encode(data);
+  }
+  console.timeEnd("encode emoji");
+
+  console.time("decode emoji");
+  for (let i = 0; i < 1000; i++) {
+    decode(encoded);
+  }
+  console.timeEnd("decode emoji");
+}
