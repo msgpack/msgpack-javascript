c5b34b74126015de6b20cdcc1902ebbe96a75a15
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-26 15:43:35 +0900

    more tests

--- test/msgpack-test-suite.test.ts
+++ test/msgpack-test-suite.test.ts
@@ -94,8 +94,10 @@ describe("msgpack-test-suite", () => {
       STR16: "a".repeat(0x100),
       STR32: "b".repeat(0x10_000),
       STR32LARGE: "c".repeat(0x100_000), // may cause "RangeError: Maximum call stack size exceeded" in simple implelementions
+      STR_INCLUDING_NUL: "foo\0bar",
       STR_BROKEN_FF: "\xff",
-      STR_LONE_SURROGATE: "\ud800",
+      STR_LONE_SURROGATE_1: "\ud800",
+      STR_LONE_SURROGATE_2: "\udbff",
       BIN16: new Uint8Array(0x100).fill(0xff),
       BIN32: new Uint8Array(0x10000).fill(0xff),
       ARRAY16: new Array<boolean>(0x100).fill(true),
