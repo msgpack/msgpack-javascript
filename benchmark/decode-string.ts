5d85f73ad234e4d677b384d02e477a0777fff37b
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-29 10:19:31 +0900

    improve: more smart selection of string decoding functions

--- benchmark/decode-string.ts
+++ benchmark/decode-string.ts
@@ -1,50 +1,45 @@
 /* eslint-disable no-console */
-import { utf8Encode, utf8Count, utf8Decode } from "../src/utils/utf8";
+import { utf8Encode, utf8Count, utf8DecodeJs, utf8DecodeTD } from "../src/utils/utf8";
 import { utf8DecodeWasm } from "../src/wasmFunctions";
 
 // @ts-ignore
 import Benchmark from "benchmark";
 
-const textDecoder = new TextDecoder();
-
-const dataSet = [10, 100, 200, 1_000, 10_000, 100_000].map((n) => {
-  return "a".repeat(n);
-});
-
-for (const str of dataSet) {
-  const byteLength = utf8Count(str);
-  const bytes = new Uint8Array(new ArrayBuffer(byteLength));
-  utf8Encode(str, bytes, 0);
-
-  console.log(`\n## string length=${str.length} byteLength=${byteLength}\n`);
-
-  const suite = new Benchmark.Suite();
-
-  const N = Math.round(100_0000 / str.length);
-
-  // use the result to avoid void-context optimizations
-  let count = 0;
-
-  suite.add("utf8Decode", () => {
-    if (utf8Decode(bytes, 0, byteLength) !== str) {
-      throw new Error("wrong result!");
-    }
+for (const baseStr of ["A", "あ", "🌏"]) {
+  const dataSet = [10, 100, 200, 1_000, 10_000, 100_000].map((n) => {
+    return baseStr.repeat(n);
   });
 
-  suite.add("utf8DecodeWasm", () => {
-    if (utf8DecodeWasm(bytes, 0, byteLength) !== str) {
-      throw new Error("wrong result!");
-    }
-  });
+  for (const str of dataSet) {
+    const byteLength = utf8Count(str);
+    const bytes = new Uint8Array(new ArrayBuffer(byteLength));
+    utf8Encode(str, bytes, 0);
 
-  suite.add("TextDecoder", () => {
-    if (textDecoder.decode(bytes.subarray(0, byteLength)) !== str) {
-      throw new Error("wrong result!");
-    }
-  });
-  suite.on("cycle", (event: any) => {
-    console.log(String(event.target));
-  });
+    console.log(`\n## string "${baseStr}" x ${str.length} (byteLength=${byteLength})\n`);
 
-  suite.run();
+    const suite = new Benchmark.Suite();
+
+    suite.add("utf8DecodeJs", () => {
+      if (utf8DecodeJs(bytes, 0, byteLength) !== str) {
+        throw new Error("wrong result!");
+      }
+    });
+
+    suite.add("utf8DecodeWasm", () => {
+      if (utf8DecodeWasm(bytes, 0, byteLength) !== str) {
+        throw new Error("wrong result!");
+      }
+    });
+
+    suite.add("TextDecoder", () => {
+      if (utf8DecodeTD(bytes, 0, byteLength) !== str) {
+        throw new Error("wrong result!");
+      }
+    });
+    suite.on("cycle", (event: any) => {
+      console.log(String(event.target));
+    });
+
+    suite.run();
+  }
 }
--- src/utils/utf8.ts
+++ src/utils/utf8.ts
@@ -83,49 +83,26 @@ export function utf8Encode(str: string, output: Uint8Array, outputOffset: number
 
 const CHUNK_SIZE = 0x10_000;
 
-export function safeStringFromCharCode(units: Array<number> | Uint16Array) {
-  if (units.length <= CHUNK_SIZE) {
-    // `String.fromCharCode.apply()` is faster than `String.fromCharCode(...units)`
-    // in case `units` is a typed array
-    return String.fromCharCode.apply(String, units as any);
-  }
-
-  let result = "";
-  for (let i = 0; i < units.length; i++) {
-    const chunk = units.slice(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE);
-    result += String.fromCharCode.apply(String, chunk as any);
-  }
-  return result;
-}
-
-const MIN_TEXT_DECODER_STRING_LENGTH = 200;
-const defaultEncoding = "utf-8";
-const sharedTextDecoder = typeof TextDecoder !== "undefined" ? new TextDecoder(defaultEncoding) : null;
-
-export function utf8Decode(bytes: Uint8Array, inputOffset: number, byteLength: number): string {
+export function utf8DecodeJs(bytes: Uint8Array, inputOffset: number, byteLength: number): string {
   let offset = inputOffset;
   const end = offset + byteLength;
 
-  if (sharedTextDecoder !== null && byteLength > MIN_TEXT_DECODER_STRING_LENGTH) {
-    const stringBytes = bytes.subarray(offset, end);
-    return sharedTextDecoder.decode(stringBytes);
-  }
-
-  const out: Array<number> = [];
+  const units: Array<number> = [];
+  let result = "";
   while (offset < end) {
     const byte1 = bytes[offset++];
     if ((byte1 & 0x80) === 0) {
       // 1 byte
-      out.push(byte1);
+      units.push(byte1);
     } else if ((byte1 & 0xe0) === 0xc0) {
       // 2 bytes
       const byte2 = bytes[offset++] & 0x3f;
-      out.push(((byte1 & 0x1f) << 6) | byte2);
+      units.push(((byte1 & 0x1f) << 6) | byte2);
     } else if ((byte1 & 0xf0) === 0xe0) {
       // 3 bytes
       const byte2 = bytes[offset++] & 0x3f;
       const byte3 = bytes[offset++] & 0x3f;
-      out.push(((byte1 & 0x1f) << 12) | (byte2 << 6) | byte3);
+      units.push(((byte1 & 0x1f) << 12) | (byte2 << 6) | byte3);
     } else if ((byte1 & 0xf8) === 0xf0) {
       // 4 bytes
       const byte2 = bytes[offset++] & 0x3f;
@@ -134,14 +111,33 @@ export function utf8Decode(bytes: Uint8Array, inputOffset: number, byteLength: n
       let unit = ((byte1 & 0x07) << 0x12) | (byte2 << 0x0c) | (byte3 << 0x06) | byte4;
       if (unit > 0xffff) {
         unit -= 0x10000;
-        out.push(((unit >>> 10) & 0x3ff) | 0xd800);
+        units.push(((unit >>> 10) & 0x3ff) | 0xd800);
         unit = 0xdc00 | (unit & 0x3ff);
       }
-      out.push(unit);
+      units.push(unit);
     } else {
-      out.push(byte1);
+      units.push(byte1);
+    }
+
+    if (units.length - 4 >= CHUNK_SIZE) {
+      result += String.fromCharCode(...units);
+      units.length = 0;
     }
   }
 
-  return safeStringFromCharCode(out);
+  if (units.length > 0) {
+    result += String.fromCharCode(...units);
+  }
+
+  return result;
+}
+
+const sharedTextDecoder = typeof TextDecoder !== "undefined" ? new TextDecoder() : null;
+export const TEXT_DECODER_AVAILABLE = process.env.TEXT_DECODER !== "never" && !!sharedTextDecoder;
+export const TEXT_DECODER_THRESHOLD = 200;
+
+export function utf8DecodeTD(bytes: Uint8Array, inputOffset: number, byteLength: number): string {
+  const stringBytes = bytes.subarray(inputOffset, inputOffset + byteLength);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return sharedTextDecoder!.decode(stringBytes);
 }
