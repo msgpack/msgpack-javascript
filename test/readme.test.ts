90bd7275320422ed4a60c58737307a8c6b050059
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-29 23:45:06 +0900

    remove tests that requires @std-proposal/temporal
    This does not work on IE11 because of ES2015 syntax

--- test/readme.test.ts
+++ test/readme.test.ts
@@ -28,41 +28,4 @@ describe("README", () => {
       deepStrictEqual(decode(encoded), object);
     });
   });
-
-  context("timestamp/temporal", () => {
-    before(function() {
-      if (typeof BigInt === "undefined") {
-        this.skip();
-      }
-    });
-
-    it("overrides timestamp-ext with std-temporal", () => {
-      const Instant = require("@std-proposal/temporal").Instant;
-
-      const extensionCodec = new ExtensionCodec();
-      extensionCodec.register({
-        type: EXT_TIMESTAMP,
-        encode: (input: any) => {
-          if (input instanceof Instant) {
-            const sec = input.seconds;
-            const nsec = Number(input.nanoseconds - BigInt(sec) * BigInt(1e9));
-            return encodeTimeSpecToTimestamp({ sec, nsec });
-          } else {
-            return null;
-          }
-        },
-        decode: (data: Uint8Array) => {
-          const timeSpec = decodeTimestampToTimeSpec(data);
-          const sec = BigInt(timeSpec.sec);
-          const nsec = BigInt(timeSpec.nsec);
-          return Instant.fromEpochNanoseconds(sec * BigInt(1e9) + nsec);
-        },
-      });
-
-      const instant = Instant.fromEpochMilliseconds(Date.now());
-      const encoded = encode(instant, { extensionCodec });
-      const decoded = decode(encoded, { extensionCodec });
-      deepStrictEqual(decoded, instant);
-    });
-  });
 });
