a10fc1cb883102e7d972e24d89d3b927009e2dd1
FUJI Goro (gfx) <gfuji@cpan.org>
2019-06-19 23:08:54 +0900

    apply prettier

--- test/encode-options.test.ts
+++ test/encode-options.test.ts
@@ -4,10 +4,7 @@ import { encode } from "@msgpack/msgpack";
 describe("encode options", () => {
   context("sortKeys", () => {
     it("canonicalize encoded binaries", () => {
-      assert.deepStrictEqual(
-        encode({ a: 1, b: 2 }, { sortKeys: true }),
-        encode({ b: 2, a: 1 }, { sortKeys: true }),
-      );
+      assert.deepStrictEqual(encode({ a: 1, b: 2 }, { sortKeys: true }), encode({ b: 2, a: 1 }, { sortKeys: true }));
     });
   });
 });
