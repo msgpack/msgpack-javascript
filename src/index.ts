395615ab19e74da0d9e76ca52e316f0e1b6c77a0
FUJI Goro (gfx) <gfuji@cpan.org>
2019-08-07 10:28:42 +0900

    tsdoc

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ export { encode } from "./encode";
 export { decode } from "./decode";
 export { decodeAsync, decodeArrayStream, decodeStream } from "./decodeAsync";
 
+/**
+ * @experimental `Decoder` is exported for experimental use.
+ */
 export { Decoder } from "./Decoder";
+
+/**
+ * @experimental `Encoder` is exported for experimental use.
+ */
 export { Encoder } from "./Encoder";
 
 // Utilitiies for Extension Types:
@@ -20,4 +27,7 @@ export {
   decodeTimestampExtension,
 } from "./timestamp";
 
+/**
+ * @experimental WebAssembly backend is experimenal.
+ */
 export { WASM_AVAILABLE as __WASM_AVAILABLE } from "./wasmFunctions";
