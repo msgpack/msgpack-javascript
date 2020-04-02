bf9a9f7150a5e75355bc3361833cf63f0a056fbe
FUJI Goro <gfuji@cpan.org>
2020-04-02 11:00:40 +0900

    fix tests. Safar does not have Blob#arrayBuffer()

--- test/decode-blob.test.ts
+++ test/decode-blob.test.ts
@@ -5,8 +5,11 @@ import { encode, decode, decodeAsync } from "@msgpack/msgpack";
 const MyBlob = typeof Blob !== "undefined" ? Blob : require("blob-polyfill").Blob;
 
 describe("Blob", () => {
-  it("decodes it with `decode()`", async () => {
+  it("decodes it with `decode()`", async function () {
     const blob = new MyBlob([encode("Hello!")]);
+    if (!blob.arrayBuffer) {
+      this.skip();
+    }
     assert.deepStrictEqual(decode(await blob.arrayBuffer()), "Hello!");
   });
 
