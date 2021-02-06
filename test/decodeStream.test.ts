617da28ac3a25aead2bdbea904055189eab54669
FUJI Goro <gfuji@cpan.org>
2021-02-06 11:32:57 +0900

    add tests and examples to decode multi-value MessagePack binaries with decodeStream()

--- test/decodeStream.test.ts
+++ test/decodeStream.test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { encode, decodeStream } from "@msgpack/msgpack";
 
 describe("decodeStream", () => {
-  it("decodes stream (array8)", async () => {
+  it("decodes stream", async () => {
     const items = [
       "foo",
       10,
@@ -12,10 +12,41 @@ describe("decodeStream", () => {
       [1, 2, 3],
     ];
 
-    const createStream = async function* () {
+    const createStream = async function* (): AsyncGenerator<Uint8Array> {
       for (const item of items) {
         yield encode(item);
       }
+    };
+
+    const result: Array<unknown> = [];
+
+    for await (const item of decodeStream(createStream())) {
+      result.push(item);
+    }
+
+    assert.deepStrictEqual(result, items);
+  });
+
+  it("decodes stream with a single binary", async () => {
+    const items = [
+      "foo",
+      10,
+      {
+        name: "bar",
+      },
+      [1, 2, 3],
+    ];
+
+    const encodedItems = items.map((item) => encode(item));
+    const encoded = new Uint8Array(encodedItems.reduce((p, c) => p + c.byteLength, 0));
+    let offset = 0;
+    for (const encodedItem of encodedItems) {
+      encoded.set(encodedItem, offset);
+      offset += encodedItem.byteLength;
+    }
+
+    const createStream = async function* (): AsyncGenerator<Uint8Array> {
+      yield encoded;
     };
 
     const result: Array<unknown> = [];
