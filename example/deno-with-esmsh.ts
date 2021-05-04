07b70b58959fa91d78f43f7be47d733d549dd040
FUJI Goro <gfuji@cpan.org>
2021-05-04 16:30:24 +0900

    add mod.ts for deno

--- example/deno-with-esmsh.ts
+++ example/deno-with-esmsh.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env deno run
 /* eslint-disable no-console */
-import * as msgpack from "https://esm.sh/@msgpack/msgpack";
+import * as msgpack from "https://esm.sh/@msgpack/msgpack/mod.ts";
 
 console.log(msgpack.decode(msgpack.encode("Hello, world!")));
