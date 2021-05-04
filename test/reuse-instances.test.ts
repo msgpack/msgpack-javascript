afd1885b4985f73ca654b11e44e016bd07d36028
FUJI Goro <gfuji@cpan.org>
2021-05-04 10:36:03 +0900

    test: run 10 times in instance-reuse tests

--- test/reuse-instances.test.ts
+++ test/reuse-instances.test.ts
@@ -7,12 +7,14 @@ const createStream = async function* (...args: any) {
   }
 };
 
+const N = 10;
+
 describe("shared instances", () => {
   context("encode() and decodeSync()", () => {
-    const encoder = new Encoder();
-    const decoder = new Decoder();
+    it("runs multiple times", () => {
+      const encoder = new Encoder();
+      const decoder = new Decoder();
 
-    it("runs #1", () => {
       const object = {
         nil: null,
         integer: 1,
@@ -24,32 +26,18 @@ describe("shared instances", () => {
         timestampExt: new Date(),
       };
 
-      const encoded: Uint8Array = encoder.encode(object);
-      deepStrictEqual(decoder.decode(encoded), object);
-    });
-
-    it("runs #2", () => {
-      const object = {
-        nil: null,
-        integer: 1,
-        float: Math.PI,
-        string: "Hello, world!",
-        binary: Uint8Array.from([1, 2, 3]),
-        array: [10, 20, 30],
-        map: { foo: "bar" },
-        timestampExt: new Date(),
-      };
-
-      const encoded: Uint8Array = encoder.encode(object);
-      deepStrictEqual(decoder.decode(encoded), object);
+      for (let i = 0; i < N; i++) {
+        const encoded: Uint8Array = encoder.encode(object);
+        deepStrictEqual(decoder.decode(encoded), object, `#${i}`);
+      }
     });
   });
 
   context("encode() and decodeAsync()", () => {
-    const encoder = new Encoder();
-    const decoder = new Decoder();
+    it("runs multiple times", async () => {
+      const encoder = new Encoder();
+      const decoder = new Decoder();
 
-    it("runs #1", async () => {
       const object = {
         nil: null,
         integer: 1,
@@ -61,32 +49,18 @@ describe("shared instances", () => {
         timestampExt: new Date(),
       };
 
-      const encoded: Uint8Array = encoder.encode(object);
-      deepStrictEqual(await decoder.decodeAsync(createStream(encoded)), object);
-    });
-
-    it("runs #2", async () => {
-      const object = {
-        nil: null,
-        integer: 1,
-        float: Math.PI,
-        string: "Hello, world!",
-        binary: Uint8Array.from([1, 2, 3]),
-        array: [10, 20, 30],
-        map: { foo: "bar" },
-        timestampExt: new Date(),
-      };
-
-      const encoded: Uint8Array = encoder.encode(object);
-      deepStrictEqual(await decoder.decodeAsync(createStream(encoded)), object);
+      for (let i = 0; i < N; i++) {
+        const encoded: Uint8Array = encoder.encode(object);
+        deepStrictEqual(await decoder.decodeAsync(createStream(encoded)), object, `#${i}`);
+      }
     });
   });
 
   context("encode() and decodeStream()", () => {
-    const encoder = new Encoder();
-    const decoder = new Decoder();
+    it("runs multiple times", async () => {
+      const encoder = new Encoder();
+      const decoder = new Decoder();
 
-    it("runs #1", async () => {
       const object = {
         nil: null,
         integer: 1,
@@ -98,40 +72,22 @@ describe("shared instances", () => {
         timestampExt: new Date(),
       };
 
-      const encoded: Uint8Array = encoder.encode(object);
-      const a: Array<any> = [];
-      for await (const item of decoder.decodeStream(createStream(encoded))) {
-        a.push(item);
+      for (let i = 0; i < N; i++) {
+        const encoded: Uint8Array = encoder.encode(object);
+        const a: Array<any> = [];
+        for await (const item of decoder.decodeStream(createStream(encoded))) {
+          a.push(item);
+        }
+        deepStrictEqual(a, [object], `#${i}`);
       }
-      deepStrictEqual(a, [object]);
-    });
-
-    it("runs #2", async () => {
-      const object = {
-        nil: null,
-        integer: 1,
-        float: Math.PI,
-        string: "Hello, world!",
-        binary: Uint8Array.from([1, 2, 3]),
-        array: [10, 20, 30],
-        map: { foo: "bar" },
-        timestampExt: new Date(),
-      };
-
-      const encoded: Uint8Array = encoder.encode(object);
-      const a: Array<any> = [];
-      for await (const item of decoder.decodeStream(createStream(encoded))) {
-        a.push(item);
-      }
-      deepStrictEqual(a, [object]);
     });
   });
 
   context("encode() and decodeArrayStream()", () => {
-    const encoder = new Encoder();
-    const decoder = new Decoder();
+    it("runs multiple times", async () => {
+      const encoder = new Encoder();
+      const decoder = new Decoder();
 
-    it("runs #1", async () => {
       const object = {
         nil: null,
         integer: 1,
@@ -143,32 +99,14 @@ describe("shared instances", () => {
         timestampExt: new Date(),
       };
 
-      const encoded: Uint8Array = encoder.encode([object]);
-      const a: Array<any> = [];
-      for await (const item of decoder.decodeStream(createStream(encoded))) {
-        a.push(item);
+      for (let i = 0; i < N; i++) {
+        const encoded: Uint8Array = encoder.encode([object]);
+        const a: Array<any> = [];
+        for await (const item of decoder.decodeStream(createStream(encoded))) {
+          a.push(item);
+        }
+        deepStrictEqual(a, [[object]], `#${i}`);
       }
-      deepStrictEqual(a, [[object]]);
-    });
-
-    it("runs #2", async () => {
-      const object = {
-        nil: null,
-        integer: 1,
-        float: Math.PI,
-        string: "Hello, world!",
-        binary: Uint8Array.from([1, 2, 3]),
-        array: [10, 20, 30],
-        map: { foo: "bar" },
-        timestampExt: new Date(),
-      };
-
-      const encoded: Uint8Array = encoder.encode([object]);
-      const a: Array<any> = [];
-      for await (const item of decoder.decodeStream(createStream(encoded))) {
-        a.push(item);
-      }
-      deepStrictEqual(a, [[object]]);
     });
   });
 });
