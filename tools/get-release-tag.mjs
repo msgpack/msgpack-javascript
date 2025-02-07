442d0350ae965e3c2dd1ebda7b700a8434417f95
FUJI Goro <g.psy.va@gmail.com>
2025-02-07 19:54:24 +0900

    fix

--- tools/get-release-tag.mjs
+++ tools/get-release-tag.mjs
@@ -2,4 +2,4 @@
 import packageJson from "../package.json" with { type: "json" };
 
 const matched = /-(beta|rc)\d+$/.exec(packageJson.version);
-console.log(matched[1] ?? "latest");
+console.log(matched?.[1] ?? "latest");
