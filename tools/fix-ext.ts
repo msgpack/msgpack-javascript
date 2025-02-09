cb2b30e3e52284febbbd6dd456ba3a65d1c40f29
FUJI Goro <g.psy.va@gmail.com>
2025-02-09 19:52:47 +0900

    setup dist.cjs

--- tools/fix-ext.ts
+++ tools/fix-ext.ts
@@ -3,22 +3,28 @@
 
 import fs from "fs";
 
-const files = process.argv.slice(2);
+const mode = process.argv[2]; // --cjs or --mjs
+const files = process.argv.slice(3);
+
+const ext = mode === "--cjs" ? "cjs" : "mjs";
+
+console.info(`Fixing ${mode} files with extension ${ext}`);
 
 for (const file of files) {
-  const fileMjs = file.replace(/\.js$/, ".mjs");
+  const fileMjs = file.replace(/\.js$/, `.${ext}`);
   console.info(`Processing ${file} => ${fileMjs}`);
   // .js => .mjs
   const content = fs.readFileSync(file).toString("utf-8");
-  const newContent = content.replace(/\bfrom "(\.\.?\/[^"]+)";/g, 'from "$1.mjs";')
+  const newContent = content.replace(/\bfrom "(\.\.?\/[^"]+).js";/g, `from "$1.${ext}";`)
+    .replace(/\brequire\("(\.\.?\/[^"]+).js"\)/g, `require("$1.${ext}");`)
     .replace(/\/\/# sourceMappingURL=(.+)\.js\.map$/,
-      "//# sourceMappingURL=$1.mjs.map");
+      `//# sourceMappingURL=$1.${ext}.map`);
   fs.writeFileSync(fileMjs, newContent);
   fs.unlinkSync(file);
 
   // .js.map => .mjs.map
   const mapping = JSON.parse(fs.readFileSync(`${file}.map`).toString("utf-8"));
-  mapping.file = mapping.file.replace(/\.js$/, ".mjs");
+  mapping.file = mapping.file.replace(/\.js$/, ext);
   fs.writeFileSync(`${fileMjs}.map`, JSON.stringify(mapping));
   fs.unlinkSync(`${file}.map`);
 }
